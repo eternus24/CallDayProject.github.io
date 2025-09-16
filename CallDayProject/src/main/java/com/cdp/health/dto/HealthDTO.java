bea53dc8367b594d350065f74c5ebd62a7ee9d32
silverJY <dyeun0717@gmail.com>
2025-09-16 11:28:18 +0900

    지영  DTO 추가

--- CallDayProject/src/main/java/com/cdp/health/dto/HealthDTO.java
+++ CallDayProject/src/main/java/com/cdp/health/dto/HealthDTO.java
@@ -1,5 +1,7 @@
 package com.cdp.health.dto;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
 
 import lombok.Getter;
@@ -11,5 +13,23 @@ import lombok.Setter;
 public class HealthDTO {
 	
 	
+	
+	
+	
+	
+	// Exercise DTO
+	
+	
+	private String exSubject;
+	private String exContent;
+	
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
+
+    private int exId;            
+    private String exPart;        // 운동 부위 
+    private String exName;        // 운동 이름
+    private String effect;        // 운동 효과
+    private String method;        // 운동 방법
 	
 }
